c45beb36162cbb9c95bc349bd961ff4ce95303e6
Hiroaki Nakamura <hnakamur@gmail.com>
2013-02-16 22:14:00 +0900

    Update hostname. Use sed instead of gawk for creating hosts file.

--- update_hosts.sh
+++ update_hosts.sh
@@ -3,8 +3,18 @@
 
 region=`ec2-metadata | sed -n 's/^local-hostname: ip-[0-9-]*\.\(.*\)\.compute\.internal/\1/p'`
 
-ec2-describe-instances --region $region -H --show-empty-fields | gawk '
-BEGIN {OFS="\t"; print "127.0.0.1", "localhost localhost.localdomain"}
-/^INSTANCE/ {ip = $18}
-/^TAG/ {print ip, gensub(/.*\tName\t([^\t]*).*/, "\\1", $0)}
+ec2-describe-instances --region $region -H --show-empty-fields | sed -n '1i\
+127.0.0.1\tlocalhost localhost.localdomain
+/^INSTANCE/{
+s/[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t//
+s/\t.*//
+h
+}
+/^TAG/{x;G;s/\n.*\tName//;s/^\([^\t]*\t[^\t]*\).*/\1/p}
 ' > /etc/hosts
+
+instance_id=`ec2-metadata | sed -n 's/^instance-id: //p'`
+tag_name=`ec2-describe-instances --region $region -H --show-empty-fields $instance_id | sed -n '/^TAG/{s/.*\tName\t//;s/\t.*//;p}'`
+
+hostname $tag_name
+sed -i 's/^HOSTNAME=.*/HOSTNAME='$tag_name'/' /etc/sysconfig/network
