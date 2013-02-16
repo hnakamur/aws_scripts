6b48748d9dd279500deeaa7f2ad99e604e36196d
Hiroaki Nakamura <hnakamur@gmail.com>
2013-02-16 22:47:33 +0900

    Remove unnecessary options.

--- update_hosts.sh
+++ update_hosts.sh
@@ -3,7 +3,7 @@
 
 region=`ec2-metadata | sed -n 's/^local-hostname: ip-[0-9-]*\.\(.*\)\.compute\.internal/\1/p'`
 
-ec2-describe-instances --region $region -H --show-empty-fields | sed -n '1i\
+ec2-describe-instances --region $region --show-empty-fields | sed -n '1i\
 127.0.0.1\tlocalhost localhost.localdomain
 /^INSTANCE/{
 s/^[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t\([^\t]*\).*/\1/
@@ -13,7 +13,7 @@ h
 ' > /etc/hosts
 
 instance_id=`ec2-metadata | sed -n 's/^instance-id: //p'`
-tag_name=`ec2-describe-instances --region $region -H --show-empty-fields $instance_id | sed -n '/^TAG/{s/.*\tName\t\([^\t]*\).*/\1/p}'`
+tag_name=`ec2-describe-instances --region $region $instance_id | sed -n '/^TAG/{s/.*\tName\t\([^\t]*\).*/\1/p}'`
 
 hostname $tag_name
 sed -i 's/^HOSTNAME=.*/HOSTNAME='$tag_name'/' /etc/sysconfig/network
